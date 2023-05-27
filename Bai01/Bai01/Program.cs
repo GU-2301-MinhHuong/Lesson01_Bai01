e68451c773bacf98c221f5d2d8ca365cee53fcc8
HuongLuuMinh <134619298+HuongLuuMinh@users.noreply.github.com>
2023-05-27 21:46:02 +0700

    Add files

---
+++ Bai01/Bai01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap gia tri canh a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nhap gia tri canh b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int cv = (a + b) * 2;
+            Console.WriteLine("Chu vi hinh chu nhat = " + cv);
+            int dt = a * b;
+            Console.WriteLine("Dien tich hinh chu nhat = " + dt);
+            Console.ReadKey();
+        }
+    }
+}
